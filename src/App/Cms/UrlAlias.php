b59905918469d861b4b20948546b59d0fd933c56
Michael Baumer <baumer@meworla.com>
2017-07-14 17:27:17 +0200

    Add getAlias for CMS routes

--- src/App/Cms/UrlAlias.php
+++ src/App/Cms/UrlAlias.php
@@ -3,7 +3,7 @@ namespace AddressixAPI\App\Cms;
 
 use AddressixAPI\App\Resource;
 
-class Article extends Resource
+class UrlAlias extends Resource
 {
   protected $resource_uri = '/cms';
   
@@ -14,18 +14,29 @@ class Article extends Resource
     $this->functions['get'] = 
       array(
         'method' => 'GET',
-        'uri' => '/articles/:id/:langcode'
+        'uri' => '/urlaliases/:id'
       );
     $this->functions['get_processed'] = 
       array(
         'method' => 'GET',
         'uri' => '/urlaliases/process'
       );
+    $this->functions['get_alias'] = 
+      array(
+        'method' => 'GET',
+        'uri' => '/urlaliases/process_alias'
+      );
   }
 
   public function getProcessed($siteid, $langcode, $alias)
   {
-    $this->request('get_processed', array('siteid'=>$siteid, 'language'=>$langcode, 'alias'=>$alias));
+    $this->request('get_processed', array('site'=>$siteid, 'language'=>$langcode, 'alias'=>$alias));
+    return $this->data;
+  }
+
+  public function getAlias($siteid, $langcode, $path)
+  {
+    $this->request('get_alias', array('site'=>$siteid, 'language'=>$langcode, 'path'=>$path));
     return $this->data;
   }
 
